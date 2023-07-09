d86ac983cbfed0638bcf93a4a49a65a403268727
Vladimir Sorokin <sorokin.dev@mail.ru>
2023-07-09 13:02:25 +0300

    Add firebase_core and run flutterfire configure

--- lib/app/di.dart
+++ lib/app/di.dart
@@ -1,9 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get_it/get_it.dart';
+
+import '../firebase_options.dart';
+import 'logger.dart';
 
 abstract class Locator {
   static final _locator = GetIt.instance;
 
-  static Future<void> init() async {}
+  static Future<void> init() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await _initFirebase();
+  }
+
+  static Future<void> _initFirebase() async {
+    logger.d('Firebase initialization started');
+
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
+    logger.d('Firebase initialized');
+  }
 
   static Future<void> dispose() async {}
 }
